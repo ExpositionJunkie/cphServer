e49f851a98deb7bf714eb653158b7b8a05fac613
Roz <rosalindavalbrecht@gmail.com>
2021-08-12 20:46:52 -0700

    playersRouter basic functionality and Vanilla Table Schemas

---
+++ models/vanillaTable.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tableSchema = new Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    level1: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    level2: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    level3: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const vanillaSchema = new Schema(
+  {
+    class: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    tableArr: [tableSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Vanilla = mongoose.model("Vanilla", vanillaSchema);
+
+module.exports = Vanilla;
--- routes/playersRouter.js
+++ routes/playersRouter.js
@@ -1,51 +1,83 @@
 const express = require("express");
+const { rawListeners } = require("../models/player");
+const Player = require("../models/player");
 const playersRouter = express.Router();
 
+// Multiple Players
 playersRouter
   .route("/")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
+  .get((req, res, next) => {
+    Player.find()
+      .then((players) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(players);
+      })
+      .catch((err) => next(err));
   })
-  .get((req, res) => {
-    res.end("Will send all the players to you.");
-  })
-  .post((req, res) => {
-    res.end(
-      `Will add the player: ${req.body.name} with description ${req.body.description}`
-    );
+  .post((req, res, next) => {
+    Player.create(req.body)
+      .then((player) => {
+        console.log("Player Created", player);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(player);
+      })
+      .catch((err) => next(err));
   })
   .put((req, res) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /players");
   })
-  .delete((req, res) => {
-    res.end("Deleting all players");
+  .delete((req, res, next) => {
+    Player.deleteMany()
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
   });
 
+// Single Player By Id
 playersRouter
   .route("/:playerId")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-  })
-  .get((req, res) => {
-    res.end(`Will send details of the player: ${req.params.playerId} to you`);
+  .get((req, res, next) => {
+    Player.findById(req.params.playerId)
+      .then((player) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(player);
+      })
+      .catch((err) => next(err));
   })
   .post((req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /players/${req.params.playerId}`);
   })
-  .put((req, res) => {
-    res.write(`Updating the player: ${req.params.playerId}\n`);
-    res.end(
-      `Will update the player: ${req.body.name} with description: ${req.body.description}`
-    );
+  .put((req, res, next) => {
+    Player.findByIdAndUpdate(
+      req.params.playerId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+      .then((player) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(player);
+      })
+      .catch((err) => next(err));
   })
-  .delete((req, res) => {
-    res.end(`Deleting player: ${req.params.playerId}`);
+  .delete((req, res, next) => {
+    Player.findByIdAndDelete(req.params.playerId)
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
   });
 
 module.exports = playersRouter;
